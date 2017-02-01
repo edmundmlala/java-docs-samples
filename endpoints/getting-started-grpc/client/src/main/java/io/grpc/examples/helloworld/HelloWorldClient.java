2c480ad4336440a976aef0b7db767dbbbb25f8bd
Jeff Mendoza <jeffmendoza@google.com>
2017-02-01 13:32:28 -0800

    Update client to do apikey

--- endpoints/getting-started-grpc/client/src/main/java/io/grpc/examples/helloworld/HelloWorldClient.java
+++ endpoints/getting-started-grpc/client/src/main/java/io/grpc/examples/helloworld/HelloWorldClient.java
@@ -31,31 +31,50 @@
 
 package io.grpc.examples.helloworld;
 
+import io.grpc.CallOptions;
+import io.grpc.Channel;
+import io.grpc.ClientCall;
+import io.grpc.ClientInterceptor;
+import io.grpc.ClientInterceptors;
+import io.grpc.ForwardingClientCall;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
+import io.grpc.Metadata;
+import io.grpc.MethodDescriptor;
 import io.grpc.StatusRuntimeException;
 
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+
 /**
  * A simple client that requests a greeting from the {@link HelloWorldServer}.
  */
 public class HelloWorldClient {
   private static final Logger logger = Logger.getLogger(HelloWorldClient.class.getName());
+  private static final String DEFAULT_ADDRESS = "localhost:50051";
 
   private final ManagedChannel channel;
   private final GreeterGrpc.GreeterBlockingStub blockingStub;
 
   /** Construct client connecting to HelloWorld server at {@code host:port}. */
-  public HelloWorldClient(String host, int port) {
-    channel = ManagedChannelBuilder.forAddress(host, port)
+  public HelloWorldClient(String address, String apiKey) {
+    channel = ManagedChannelBuilder.forTarget(address)
         // Channels are secure by default (via SSL/TLS). For the example we disable TLS to avoid
         // needing certificates.
         .usePlaintext(true)
         .build();
-    blockingStub = GreeterGrpc.newBlockingStub(channel);
+    Channel ch = ClientInterceptors.intercept(channel,  new Interceptor(apiKey));
+
+    blockingStub = GreeterGrpc.newBlockingStub(ch);
   }
 
   public void shutdown() throws InterruptedException {
@@ -76,21 +95,100 @@ public class HelloWorldClient {
     logger.info("Greeting: " + response.getMessage());
   }
 
+  private static final class Interceptor implements ClientInterceptor {
+    private final String apiKey;
+
+    private static Logger LOGGER = Logger.getLogger("InfoLogging");
+
+    private static Metadata.Key<String> API_KEY_HEADER =
+        Metadata.Key.of("x-api-key", Metadata.ASCII_STRING_MARSHALLER);
+
+    public Interceptor(String apiKey) {
+      this.apiKey = apiKey;
+    }
+
+    @Override
+    public <ReqT, RespT> ClientCall<ReqT, RespT> interceptCall(
+        MethodDescriptor<ReqT,RespT> method, CallOptions callOptions, Channel next) {
+      LOGGER.info("Intercepted " + method.getFullMethodName());
+      ClientCall<ReqT, RespT> call = next.newCall(method, callOptions);
+
+      call = new ForwardingClientCall.SimpleForwardingClientCall<ReqT, RespT>(call) {
+        @Override
+        public void start(Listener<RespT> responseListener, Metadata headers) {
+          if (apiKey != null && !apiKey.isEmpty()) {
+            LOGGER.info("Attaching API Key: " + apiKey);
+            headers.put(API_KEY_HEADER, apiKey);
+          }
+          super.start(responseListener, headers);
+        }
+      };
+      return call;
+    }
+  }
+
   /**
    * Greet server. If provided, the first element of {@code args} is the name to use in the
    * greeting.
    */
   public static void main(String[] args) throws Exception {
-    HelloWorldClient client = new HelloWorldClient(args[0], Integer.parseInt(args[1]));
+    Options options = createOptions();
+    CommandLineParser parser = new DefaultParser();
+    CommandLine params;
     try {
-      /* Access a service running on the local machine on port 50051 */
-      String user = "world";
-      if (args.length > 2) {
-        user = args[2]; /* Use the arg as the name to greet if provided */
-      }
-      client.greet(user);
+      params = parser.parse(options, args);
+    } catch (ParseException e) {
+      System.err.println("Invalid command line: " + e.getMessage());
+      printUsage(options);
+      return;
+    }
+
+    String address = params.getOptionValue("host", DEFAULT_ADDRESS);
+    String apiKey = params.getOptionValue("api_key");
+    String greetee = params.getOptionValue("greetee", "world");
+
+    HelloWorldClient client = new HelloWorldClient(address, apiKey);
+    try {
+      client.greet(greetee);
     } finally {
       client.shutdown();
     }
   }
+
+  private static Options createOptions() {
+    Options options = new Options();
+
+    options.addOption(Option.builder()
+        .longOpt("host")
+        .desc("The address of the gRPC server")
+        .hasArg()
+        .argName("host")
+        .type(String.class)
+        .build());
+
+    options.addOption(Option.builder()
+        .longOpt("api_key")
+        .desc("The API key to use for RPC calls")
+        .hasArg()
+        .argName("key")
+        .type(String.class)
+        .build());
+
+    options.addOption(Option.builder()
+        .longOpt("greetee")
+        .desc("Who or what to greet")
+        .hasArg()
+        .argName("greetee")
+        .type(String.class)
+        .build());
+
+    return options;
+  }
+
+  private static void printUsage(Options options) {
+    HelpFormatter formatter = new HelpFormatter();
+    formatter.printHelp("client",
+        "A simple gRPC client for use with Endpoints.", options, "", true);
+  }
+
 }
